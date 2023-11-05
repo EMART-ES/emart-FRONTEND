55eb0c149775eacce961f4c25745802416bec093
matheuxsx3 <matheus.s280604@gmail.com>
2023-11-05 14:05:36 -0300

    ajustando imagem

--- src/components/Home/navbar/Navbar.tsx
+++ src/components/Home/navbar/Navbar.tsx
@@ -4,13 +4,15 @@ export default function Navbar() {
     return (
         <>
             <nav>
-                <div className='logo'><img src="" alt="" /></div>
-                <ul className='nav-ul'>
-                <li className='nav-li'><a href="#">Início</a></li>
-                <li><a href="#">Perfil</a></li>
-                <li><a href="#">Notificações</a></li>
-                <li><a href="#">Configurações</a></li>
-                <li><a href="#">Suporte</a></li>
-            </ul></nav>
+                <div className='menu'><button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                </svg></button></div>
+                <div className='logo'><a href="logo"><img src="https://github.com/EMART-ES/emart-FRONTEND/blob/main/public/img/Logo_c_fundo.jpeg?raw=true" alt="logo da emart com fundo" /></a></div>
+
+                <div className='carrinho'><button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart" viewBox="0 0 16 16">
+                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                </svg></button>
+                </div>
+            </nav>
         </>)
 };
