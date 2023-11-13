6ea9adf766a04cf85c1ffae785b3eeec0b085a34
matheuxsx3 <matheus.s280604@gmail.com>
2023-11-12 23:50:03 -0300

    adicionando tailwind css

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,15 @@
 import './Home.css';
-import Card from "./cards/Card";
-import Navbar from "./navbar/Navbar";
+import Card from "../../components/cards/Card";
+import Navbar from "../../components/navbar/Navbar";
 
 export default function Home() {
     return (
         <>
             <header><Navbar /></header>
-            <section><h1>Venha fazer parte da nossa comunidade!</h1></section>
-            <section className='card-container'>
+            <section className="text-center">
+                <h1>Venha fazer parte da nossa comunidade!</h1>
+            </section>
+            <section className='flex flex-wrap justify-center'>
 
                 <Card id={1} image='https://i.pinimg.com/736x/63/56/65/635665870427679b47dda476e1d536c3.jpg' price={200} title='Camisa lacostada' />
                 <Card id={2} image='https://i.pinimg.com/736x/63/56/65/635665870427679b47dda476e1d536c3.jpg' price={200} title='Camisa lacostada' />
